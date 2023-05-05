3a7c2e97cacd8fb696522ea70832cd0571a25094
Antonio-Italiano <antoitalia221@gmail.com>
2023-05-05 17:30:41 +0200

    fix Snack13

--- src/org/javasnacks/java/Snack13.java
+++ src/org/javasnacks/java/Snack13.java
@@ -11,13 +11,23 @@ public class Snack13 {
 		Integer[] copyArr1 = new Integer[arr1.length];
 		Integer[] copyArr2 = new Integer[arr2.length];
 		
-		for (int i = 1; i < arr1.length; i++) {  
-			copyArr1[0] = arr2[0];
+		for (int i = 0; i < arr1.length; i++) { 
+			if(copyArr1[0] != arr2[0]) {				
+				copyArr1[0] = arr2[0];
+			} else {				
 			copyArr1[i] = arr1[i];		
+			}
 		}
 		for (int i = 0; i < arr2.length; i++) {  
-			copyArr2[0] = arr1[0];		
-			copyArr2[i] = arr2[i];
+			if(copyArr2[0] != arr1[0]) {				
+				copyArr2[0] = arr1[0];	
+			} else {				
+				copyArr2[i] = arr2[i];	
+			}
+			
+				
+			
+			
 		}
 		
 		System.out.println(Arrays.asList(copyArr1));
